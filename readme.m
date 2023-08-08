d30e75c483574c03463ae46d133b2bba9c52e953
Ruano Rungainis <141497938+Ruano8800@users.noreply.github.com>
2023-08-09 02:09:18 +0300

    simnet new fixes

---
+++ readme.m
@@ -0,0 +1,53 @@
+% Unzip all files in Matlab current directory, then type "handrec"
+% on Matlab command window. A simple and intuitive GUI should appear.
+% 
+% GUI FUNCTIONS
+% 
+% SELECT IMAGE
+% Read the input image
+%
+% ADD SELECTED IMAGE TO DATABASE
+% Input image is added to database and will be used for training. An
+% integer ID is required. ID is a progressive integer number associated to
+% hand.
+%
+% DATABASE INFO
+% Show informations about the images present in database.
+%
+% HAND GEOMETRY RECOGNITION
+% Hand matching. Selected input image is compared with all hand images
+% present in database.
+%
+% DELETE DATABASE
+% Remove database from the current directory.
+%
+% INFO
+% Show informations about this software.
+%
+% FEATURE VISUALIZATION
+% Visualization utility.
+%
+% SOURCE CODE FOR HAND GEOMETRY RECOGNITION SYSTEM
+% How to obtain the complete source code.
+%
+% EXIT
+% Quit program.
+%
+% 
+% HAND GEOMETRY RECOGNITION SYSTEM V3
+%
+% In order to obtain the complete source code please visit
+% 
+% http://matlab-recognition-code.com/hand-geometry-recognition-system-matlab-full-source-code/
+%
+% For any question please email me hamdouchhd@hotmail.com
+% 
+% Hamdi Boukamcha
+% Sousse
+% 4081
+% Tunisia
+% email hamdouchhd@hotmail.com
+% mobile +21650674269
+% website http://matlab-recognition-code.com
+%
+%
---
+++ sourcecode.m
@@ -0,0 +1,17 @@
+% 
+% HAND GEOMETRY RECOGNITION SYSTEM V3
+%
+% In order to obtain the complete source code please visit
+% 
+% http://matlab-recognition-code.com/hand-geometry-recognition-system-matlab-full-source-code/
+%
+% For any question please email me hamdouchhd@hotmail.com
+% 
+% Hamdi Boukamcha
+% Sousse
+% 4081
+% Tunisia
+% email hamdouchhd@hotmail.com
+% mobile +21650674269
+% website http://matlab-recognition-code.com
+%
